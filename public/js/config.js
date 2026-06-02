e39b9bcffb0840f516e7f2c506d7fd106848ce7c
morish276 <moraon276@gmail.com>
2026-06-02 13:04:16 +0530

    Update TURN server credentials to private Metered account

--- public/js/config.js
+++ public/js/config.js
@@ -10,15 +10,26 @@ const TOSTREAM_CONFIG = {
   ICE_SERVERS: [
     { urls: 'stun:stun.l.google.com:19302' },
     { urls: 'stun:stun1.l.google.com:19302' },
+    { urls: 'stun:stun.relay.metered.ca:80' },
     {
-      urls: 'turn:openrelay.metered.ca:80',
-      username: 'openrelayproject',
-      credential: 'openrelayproject'
+      urls: 'turn:global.relay.metered.ca:80',
+      username: '240a091f42e6992f1ae1b188',
+      credential: 'N9LnldFl4IVh6jhY',
     },
     {
-      urls: 'turn:openrelay.metered.ca:443?transport=tcp',
-      username: 'openrelayproject',
-      credential: 'openrelayproject'
+      urls: 'turn:global.relay.metered.ca:80?transport=tcp',
+      username: '240a091f42e6992f1ae1b188',
+      credential: 'N9LnldFl4IVh6jhY',
+    },
+    {
+      urls: 'turn:global.relay.metered.ca:443',
+      username: '240a091f42e6992f1ae1b188',
+      credential: 'N9LnldFl4IVh6jhY',
+    },
+    {
+      urls: 'turns:global.relay.metered.ca:443?transport=tcp',
+      username: '240a091f42e6992f1ae1b188',
+      credential: 'N9LnldFl4IVh6jhY',
     }
   ]
 };
